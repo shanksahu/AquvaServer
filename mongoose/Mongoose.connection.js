77590b1394b46e74aadf96f457ff7469d587ecd4
Shashank <shankgithub@gmail.com>
2021-12-29 12:51:31 +0530

    first commit

--- mongoose/Mongoose.connection.js
+++ mongoose/Mongoose.connection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-dotenv.config({path: "./config.env"})
+// dotenv.config({path: "./config.env"})
 
-const database = mongoose.connect(process.env.DATABASE,{
+const database = mongoose.connect("mongodb+srv://shank:shank@cluster0.4ws36.mongodb.net/login?retryWrites=true&w=majority",{
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=>{
         console.log("Database is connected");
